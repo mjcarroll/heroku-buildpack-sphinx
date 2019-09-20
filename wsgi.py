7c456b5b832614e1fc4d2fe2c943e4978362c7b7
Michael Carroll <michael@openrobotics.org>
2019-09-20 10:12:43 -0500

    Change build dir.

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
 import os
 import static
 
-with open('/app/docs/.build-dir') as fb:
-    build_dir = fb.read().strip()
-
-build_path = os.path.join('/app/docs', build_dir, 'html')
-application = static.Cling(build_path)
+application = static.Cling('/app/build/html')
